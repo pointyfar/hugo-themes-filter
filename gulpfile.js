b5fea9d5c0d137f6bb1e60037ce89f6b7f69590b
Farina Pontejos <pointyfar@gmail.com>
2018-04-22 23:58:59 +1000

    added splashy for color dots

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gutil = require('gulp-util');
 var toml = require('toml');
 var del = require("del");
 var rename = require("gulp-rename");
-
+var splashy = require('splashy')();
 
 var themesPath = 'all-hugo-themes';
 var themesImgPath = './static/theme-images';
@@ -14,6 +14,8 @@ var themesJsonPath = './data/themes.json';
 var THEMEBUNDLE = {
   themes : []
 };
+
+var themePromises = [];
 
 var exclude_dirs = [
   '_script',
@@ -54,6 +56,7 @@ function copyImage(path, theme) {
 
 gulp.task('themes:assemble', function(done) {
   var folders = getFolders(themesPath);
+  var counter = 0;
   var tasks = folders.map(function(folder) {
     if (folder.length === 0) {
       return done(); 
@@ -65,28 +68,48 @@ gulp.task('themes:assemble', function(done) {
       var themetoml = fs.readFileSync( themePath , 'utf8');
       var themejson = getToml(themetoml);
       themejson['path'] = folder;
-      
+      var color = []
+      themejson['colors'] = []
+        
       if (fs.existsSync(imgPath)) {
         themejson['hasimage'] = true;
-        // copyImage(imgPath, folder);
-      } 
-      
-      THEMEBUNDLE.themes.push(themejson);
+        themePromises.push(getColor(imgPath, themejson));
+      } else {
+        themePromises.push(Promise.resolve(themejson))
+      }
+      // copyImage(imgPath, folder);
+        
     }
   });
 
   done();
 });
 
-gulp.task('themes:write', function(done){
-  var content = JSON.stringify(THEMEBUNDLE);
-  fs.writeFile(themesJsonPath, content, 'utf8', function (err) {
-    if (err) {
-        return gutil.log(err)
-    }
-  })
+function getColor(imgPath, tj){
+    return splashy.fromFile(imgPath)
+                  .then(function(col) {
+                    tj['colors'] = col;
+                    return tj
+                  });
+}
 
-  done();
+gulp.task('themes:write', function(done){
+  
+  Promise.all(themePromises)
+          .then( th => {
+            THEMEBUNDLE.themes = th;
+            // gutil.log('res', th)
+            return JSON.stringify(THEMEBUNDLE);
+          })
+          .then( res => {
+            fs.writeFile(themesJsonPath, res, 'utf8', function (err) {
+              if (err) {
+                return gutil.log(err)
+              }
+            })
+          })
+          .then( d => {done()});
+
 });
 
 gulp.task("themes:clean", function(done){
